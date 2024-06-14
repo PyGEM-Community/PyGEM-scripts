492c40a2a8c369bd8eae5670e331780095aca4f0
btobers <tobers.brandon@gmail.com>
2024-06-14 16:14:54 -0400

    cleanup args

--- duplicate_gdirs.py
+++ duplicate_gdirs.py
@@ -5,12 +5,12 @@ import pygem_input as pygem_prms
 
 
 def main():
-    parser = argparse.ArgumentParser(description="make duplicate oggm glacier directories")
+    parser = argparse.ArgumentParser(description="Script to make duplicate oggm glacier directories - primarily to avoid corruption if parellelizing runs on a single glacier")
     # add arguments
     parser.add_argument('-rgi_glac_number', type=str, default=None,
                         help='Randoph Glacier Inventory region')
     parser.add_argument('-num_copies', type=int, default=1,
-                        help='Randoph Glacier Inventory region')
+                        help='Number of copies to create of the glacier directory data')
     args = parser.parse_args()
     num_copies = args.num_copies
     glac_num = args.rgi_glac_number
